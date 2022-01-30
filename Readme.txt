60438ec85d0fc581322d90de6488222bed1ac003
aizen7987 <94191135+aizen7987@users.noreply.github.com>
2022-01-29 23:04:08 -0600

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,50 @@
+Pasos para verificar el testproject
+
+instalacion de programas 
+
+xammp
+
+descargarlo de la pagina del proveedor
+
+instalar siguiente y ultimo finalizar 
+
+pedira permisos se los otorgamos
+1
+para visualizar el mockup debemos seleccionar la carpeta mockup 
+click en index.html 
+se abrira el navegador
+
+
+2
+--instalacion de projectest en laravel-- 
+
+descomprimir el archivo dividido en 2 partes 
+projectest.part1
+projectest.part2
+
+copiar la carpeta projectest en la ruta 
+C:\xampp\htdocs 
+
+-- instalacion de base de datos--
+abrimos el navegador y colocamos localhost/phpmyadmin
+Dar click en el boton de importar 
+seleccionamos en elegir archivo 
+buscamos la base de datos projectest.sql proporcionada en el proyecto
+luego seleccionamos el boton continuar
+
+la base de datos estara integrada correctamente.
+
+ingresar al navegador y colocar la direccion localhost/projectest/public 
+loguiarnos con correo y contraseña
+
+credenciales
+correo admin@admin.com
+contraseña 12345
+
+3
+--instalacion del decodificador--
+
+copiar la carpeta cadena 
+C:\xampp\htdocs 
+ingresar al navegador y colocar la direccion localhost/cadena
+
---
+++ projectest.sql
@@ -0,0 +1,129 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.4.17-MariaDB : Database - projectest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`projectest` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `projectest`;
+
+/*Table structure for table `empleados` */
+
+DROP TABLE IF EXISTS `empleados`;
+
+CREATE TABLE `empleados` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellido` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `correo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `empleados` */
+
+insert  into `empleados`(`id`,`nombre`,`apellido`,`correo`,`pass`,`created_at`,`updated_at`) values (2,'oscar','viera','oscar@gmail.com','1234568',NULL,'2022-01-28 04:11:57'),(3,'miguel','ramos','miguel@gmail.com','12345',NULL,NULL),(9,'pedro','flores','pedro@gmail.com','12345',NULL,NULL);
+
+/*Table structure for table `failed_jobs` */
+
+DROP TABLE IF EXISTS `failed_jobs`;
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `failed_jobs` */
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`id`,`migration`,`batch`) values (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2019_08_19_000000_create_failed_jobs_table',1),(4,'2019_12_14_000001_create_personal_access_tokens_table',1),(5,'2022_01_27_164646_create_empleados_table',2);
+
+/*Table structure for table `password_resets` */
+
+DROP TABLE IF EXISTS `password_resets`;
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `password_resets` */
+
+/*Table structure for table `personal_access_tokens` */
+
+DROP TABLE IF EXISTS `personal_access_tokens`;
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `personal_access_tokens` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`) values (5,'oscar','oscar@gmail.com',NULL,'$2y$10$w7fioeCW87G90vZmO3EY2OxtFGzBQXO/QfT2pdiZ4NprpXF6yTq8C',NULL,'2022-01-29 04:41:17','2022-01-29 04:41:17'),(6,'juan pez','juanpez@gmail.com',NULL,'$2y$10$/TIusl70P8w9SRM9vDdVMu3HHivH8KvUyJYrX54Bwh8CW2R1NsZc6',NULL,'2022-01-29 05:16:48','2022-01-29 05:16:48'),(7,'Frank','franciscosalgado9105@outlook.com',NULL,'$2y$10$TZF4AC2xR9EEjl/7zMqkxOfUGL8KcNQhvC5WJBfgQPGWiw3k5fMJy',NULL,'2022-01-30 03:25:48','2022-01-30 03:38:31'),(8,'admin','admin@admin.com',NULL,'$2y$10$qTU9/UGcsY3EFVdtpxwmu.San6F8K2yFEjvKAwjWc3FpX5.GvdHTm',NULL,'2022-01-30 04:34:23','2022-01-30 04:34:23');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
